681219df37e21ca312a9f0493eaaeaaaa98c2360
A-Machagiri <112374637+A-Machagiri@users.noreply.github.com>
2024-02-14 20:48:03 +0530

    Update proj2.py

--- proj2.py
+++ proj2.py
@@ -1,21 +1,82 @@
 import streamlit as st
 import yolov5 as yl
-from streamlit_webrtc import webrtc_streamer
+import av
+
+import sqlite3
+from streamlit_webrtc import webrtc_streamer,WebRtcMode,RTCConfiguration
 import pandas as pd
 from form2 import data, sum,exce
 from image import imag
+from Controller.sequel import *
+from Controller.userController import *
 from io import BytesIO
 from PIL import Image
+import time
+import torch
 import numpy as np
-def call():
-    th=st.sidebar.slider("Confidence threshold",min_value=0.00,max_value=1.00, step=0.15, value=0.65)
+def model():
+    return yl.load("best.pt")
+model=model()
+class Videoprocessor:
+    def __init__(self):
+        self.res=None
+        self.confidence=0.5
+
+    def getRes(self):
+        #time.sleep(5)
+        return self.res
+
+    def recv(self, frame):
+        model.conf=self.confidence
+        frm = frame.to_ndarray(format="bgr24")
+        
+        # vision processing
+        flipped = frm[:, ::-1, :]
+
+        # model processing
+        im_pil = Image.fromarray(flipped)
+        results = model(im_pil, size=112)
+        self.res=results
+        bbox_img = np.array(results.render()[0])
+        return av.VideoFrame.from_ndarray(bbox_img, format="bgr24")
 def staff():
-    st.sidebar.date_input("Date")
-    call()
-    sel = st.sidebar.radio("View Mode", ["None", "📹video", "📊data", "📷image"])
+    with st.sidebar:
+        date=st.date_input("Date")
+        confidence=st.slider("Confidence threshold", min_value=0.00,max_value=1.00, step=0.15, value=0.65)
+        sel = st.radio("View Mode", ["None", "📹video", "📊data", "📷image"])
     if (sel == "📹video"):
         st.title("📹 Objects deletion Model")
-        webrtc_streamer(key="example")
+        webrtc_ctx = webrtc_streamer(
+                key="webcam",
+                mode=WebRtcMode.SENDRECV,
+                
+                media_stream_constraints={"video": True, "audio": False},
+                video_processor_factory=Videoprocessor,
+                async_processing=True,
+            )
+        if webrtc_ctx.state.playing:
+                webrtc_ctx.video_processor.confidence=confidence
+        if st.checkbox('Show the detected labels'):
+            empty=st.empty()
+            store=st.button('Store')
+            if webrtc_ctx.state.playing:
+                while True:
+                    if webrtc_ctx.video_processor:
+                        results = webrtc_ctx.video_processor.getRes()
+                        if results != None:
+                            count = results.pandas().xyxy[0]
+                            dj = count["name"].tolist()
+                            dj = read_df(dj)
+
+                            empty.table(dj)
+                            for idx,row in dj.iterrows():
+                                if store:
+                                    Insert(date, row["Name"], int(row["Count"]))
+                                    time.sleep(3)
+                        else:
+                            empty.write("No labels detected")
+                    else:
+                        break
     if(sel=="📷image"):
         imag()
     if (sel == "📊data"):
